e3d673966b606fcd6778ab5d0185cc260ec1e72a
Felipe Torres González <felipe.torresgonzalez@ess.eu>
2020-04-22 13:24:37 +0200

    src: component definition moved to the main pkg

--- fpga/srcs/vhdl/evr_pkg.vhd
+++ fpga/srcs/vhdl/evr_pkg.vhd
@@ -16,6 +16,97 @@ use ieee.std_logic_arith.all;
 use ieee.numeric_std.all;
 
 package evr_pkg is
+  component evr_dc is
+      generic (
+    -- MGT RX&TX signal pair polarity
+    RX_POLARITY                  : std_logic := '0'; -- '1' for inverted polarity
+    TX_POLARITY                  : std_logic := '0'; -- '1' for inverted polarity
+    -- MGT reference clock selection
+    REFCLKSEL                    : std_logic := '0' -- 0 - REFCLK0, 1 - REFCLK1
+    );
+  port (
+    -- System bus clock
+    sys_clk         : in std_logic;
+    refclk_out      : out std_logic; -- Reference clock output
+    event_clk_out   : out std_logic; -- Event clock output, delay compensated
+                                     -- and locked to EVG
+
+    -- Receiver side connections
+    event_rxd       : out std_logic_vector(7 downto 0);  -- Received event code
+    dbus_rxd        : out std_logic_vector(7 downto 0);  -- Distributed bus data
+    databuf_rxd     : out std_logic_vector(7 downto 0);  -- Databuffer data
+    databuf_rx_k    : out std_logic; -- Databuffer K-character
+    databuf_rx_ena  : out std_logic; -- Databuf data enable
+    databuf_rx_mode : in std_logic;  -- Databuf receive mode, '1' enabled, '0'
+                                     -- disabled (only for non-DC)
+    dc_mode         : in std_logic;  -- Delay compensation mode enable
+
+    rx_link_ok      : out   std_logic; -- Received link ok
+    rx_violation    : out   std_logic; -- Receiver violation detected
+    rx_clear_viol   : in    std_logic; -- Clear receiver violatio flag
+
+    -- Transmitter side connections
+    event_txd       : in  std_logic_vector(7 downto 0); -- TX event code
+    dbus_txd        : in  std_logic_vector(7 downto 0); -- TX distributed bus data
+    databuf_txd     : in  std_logic_vector(7 downto 0); -- TX databuffer data
+    databuf_tx_k    : in  std_logic; -- TX databuffer K-character
+    databuf_tx_ena  : out std_logic; -- TX databuffer data enable
+    databuf_tx_mode : in  std_logic; -- TX databuffer transmit mode, '1'
+                                     -- enabled, '0' disabled
+
+    reset           : in  std_logic; -- Transmitter reset
+
+    -- Delay compensation signals
+    delay_comp_update : in std_logic;
+    delay_comp_value  : in std_logic_vector(31 downto 0);
+    delay_comp_target : in std_logic_vector(31 downto 0);
+    delay_comp_locked_out : out std_logic;
+
+    -- MGT physical pins
+    MGTREFCLK0_P : in std_logic;
+    MGTREFCLK0_N : in std_logic;
+    MGTREFCLK1_P : in std_logic;   -- JX3 pin 2,   Zynq U5
+    MGTREFCLK1_N : in std_logic;   -- JX3 pin 3,   Zynq V5
+
+    MGTTX2_P     : out std_logic;  -- JX3 pin 25,  Zynq AA5
+    MGTTX2_N     : out std_logic;  -- JX3 pin 27,  Zynq AB5
+    MGTRX2_P     : in std_logic;   -- JX3 pin 20,  Zynq AA9
+    MGTRX2_N     : in std_logic    -- JX3 pin 22,  Zynq AB9
+  );
+  end component;
+
+  component databuf_rx_dc is
+    port (
+      -- Memory buffer RAMB read interface
+      data_out          : out std_logic_vector(31 downto 0);
+      size_data_out     : out std_logic_vector(31 downto 0);
+      addr_in           : in std_logic_vector(10 downto 2);
+      clk               : in std_logic;
+
+      -- Data stream interface
+      databuf_data      : in std_logic_vector(7 downto 0);
+      databuf_k         : in std_logic;
+      databuf_ena       : in std_logic;
+      event_clk         : in std_logic;
+
+      delay_comp_update : out std_logic;
+      delay_comp_rx     : out std_logic_vector(31 downto 0);
+      delay_comp_status : out std_logic_vector(31 downto 0);
+      topology_addr     : out std_logic_vector(31 downto 0);
+
+      -- Control interface
+      irq_out           : out std_logic;
+
+      sirq_ena          : in std_logic_vector(0 to 127);
+      rx_flag           : out std_logic_vector(0 to 127);
+      cs_flag           : out std_logic_vector(0 to 127);
+      ov_flag           : out std_logic_vector(0 to 127);
+      clear_flag        : in std_logic_vector(0 to 127);
+
+      reset             : in std_logic
+    );
+  end component;
+
   type integer_array is array (integer range <>) of integer;
   constant EVENT_RATE          : integer := 125000000;
   constant MGT_RX_PRESCALER    : integer := 1024;
@@ -59,7 +150,7 @@ package evr_pkg is
   constant C_SIGNAL_MAP_CLK    : integer := 60;
   constant C_SIGNAL_MAP_ICLK   : integer := 59;
   type signal_map_vector is array (integer range<>) of std_logic_vector(C_SIGNAL_MAP_BITS-1 downto 0);
-  -- C_EVR_REG_CMPMIN defines the first address bit to compare 
+  -- C_EVR_REG_CMPMIN defines the first address bit to compare
   constant C_EVR_REG_CMP_LOW          : integer := 14;
   constant C_EVR_REG_STATUS           : std_logic_vector(0 to 31) := X"00000000";
   constant C_EVR_REG_CONTROL          : std_logic_vector(0 to 31) := X"00000004";
@@ -111,27 +202,27 @@ package evr_pkg is
   constant C_EVR_REG_DBUS_CMP_HIGH    : integer := 24; -- last address bit to compare
   constant C_EVR_REG_DBUS_INDEX_LOW   : integer := 27;
   constant C_EVR_REG_DBUS_INDEX_HIGH  : integer := 29;
-  constant C_EVR_REG_PULSE_BASE       : std_logic_vector(0 to 31) := X"00000200"; 
+  constant C_EVR_REG_PULSE_BASE       : std_logic_vector(0 to 31) := X"00000200";
   constant C_EVR_REG_PULSE_CMP_HIGH   : integer := 22; -- last address bit to compare
   constant C_EVR_REG_PULSE_INDEX_LOW  : integer := 23;
   constant C_EVR_REG_PULSE_INDEX_HIGH : integer := 27;
-  constant C_EVR_REG_FPOUT_BASE       : std_logic_vector(0 to 31) := X"00000400"; 
+  constant C_EVR_REG_FPOUT_BASE       : std_logic_vector(0 to 31) := X"00000400";
   constant C_EVR_REG_FPOUT_CMP_HIGH   : integer := 25; -- last address bit to compare
   constant C_EVR_REG_FPOUT_INDEX_LOW  : integer := 27;
   constant C_EVR_REG_FPOUT_INDEX_HIGH : integer := 29;
-  constant C_EVR_REG_UNIV_BASE        : std_logic_vector(0 to 31) := X"00000440"; 
+  constant C_EVR_REG_UNIV_BASE        : std_logic_vector(0 to 31) := X"00000440";
   constant C_EVR_REG_UNIV_CMP_HIGH    : integer := 25; -- last address bit to compare
   constant C_EVR_REG_UNIV_INDEX_LOW   : integer := 26;
   constant C_EVR_REG_UNIV_INDEX_HIGH  : integer := 29;
-  constant C_EVR_REG_TBOUT_BASE       : std_logic_vector(0 to 31) := X"00000480"; 
+  constant C_EVR_REG_TBOUT_BASE       : std_logic_vector(0 to 31) := X"00000480";
   constant C_EVR_REG_TBOUT_CMP_HIGH   : integer := 25; -- last address bit to compare
   constant C_EVR_REG_TBOUT_INDEX_LOW  : integer := 26;
   constant C_EVR_REG_TBOUT_INDEX_HIGH : integer := 29;
-  constant C_EVR_REG_BPOUT_BASE       : std_logic_vector(0 to 31) := X"000004C0"; 
+  constant C_EVR_REG_BPOUT_BASE       : std_logic_vector(0 to 31) := X"000004C0";
   constant C_EVR_REG_BPOUT_CMP_HIGH   : integer := 25; -- last address bit to compare
   constant C_EVR_REG_BPOUT_INDEX_LOW  : integer := 26;
   constant C_EVR_REG_BPOUT_INDEX_HIGH : integer := 29;
-  constant C_EVR_REG_FPIN_BASE        : std_logic_vector(0 to 31) := X"00000500"; 
+  constant C_EVR_REG_FPIN_BASE        : std_logic_vector(0 to 31) := X"00000500";
   constant C_EVR_REG_FPIN_CMP_HIGH    : integer := 25; -- last address bit to compare
   constant C_EVR_REG_FPIN_INDEX_LOW   : integer := 29;
   constant C_EVR_REG_FPIN_INDEX_HIGH  : integer := 29;
@@ -359,7 +450,7 @@ package evr_pkg is
   constant C_EVR_FPIN_BACKLEV_ENABLE  : integer := 28;
   constant C_EVR_FPIN_EXTLEV_ACT      : integer := 29;
   constant C_EVR_FPIN_INPUT_STATE     : integer := 31;
-  
+
   constant C_DIAG_REG_CNT_CMP_HIGH    : integer := 24;
   constant C_DIAG_COUNTERS_INDEX_LOW  : integer := 25;
   constant C_DIAG_COUNTERS_INDEX_HIGH : integer := 29;
