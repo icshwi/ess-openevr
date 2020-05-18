78c216a6782b9f9ec0f0a50a91c14a76692424d7
Ross Elliot <ross.elliot@esss.se>
2020-05-18 15:57:28 +0200

    Add debug port width to block mask

--- xgui/ess_openEVR_v0_1.tcl
+++ xgui/ess_openEVR_v0_1.tcl
@@ -1,10 +1,8 @@
 # Definitional proc to organize widgets for parameters.
 proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
-  #Adding Page
-  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
-  ipgui::add_param $IPINST -name "g_DEBUG_WIDTH" -parent ${Page_0}
-
+  set g_DEBUG_WIDTH [ipgui::add_param $IPINST -name "g_DEBUG_WIDTH"]
+  set_property tooltip {Width of the debug port} ${g_DEBUG_WIDTH}
 
 }
 
